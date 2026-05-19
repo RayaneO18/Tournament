2fddfba938ef5c344fbb433380698222bdf074e6
Ouaani Rayane <ouaani.rayane@gmail.com>
2026-05-19 19:27:53 +0200

    Si clique sur la page active, on force le reload

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,43 +14,29 @@ const tabs = [
 
 export default function Navbar() {
   const pathname = usePathname();
-  // const handleRefresh = () => {
-  //   window.location.reload();};
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.navInner}>
         {tabs.map((tab) => {
           const active = pathname === tab.href;
+          
           return (
-            <Link key={tab.href} href={tab.href} className={styles.navLink}>
+            <Link 
+              key={tab.href} 
+              href={tab.href} 
+              className={styles.navLink}
+              onClick={(e) => {
+                if (active) {
+                  e.preventDefault
+                  window.location.reload(); 
+                }
+              }}
+            >
               <div className={active ? styles.activeText : styles.inactiveText}>
                 {tab.name}
                 {active && <div className={styles.underline} />}
               </div>
-        {/* <button 
-      onClick={handleRefresh}
-      style={{
-        position: 'fixed',
-        bottom: '20px',
-        right: '20px',
-        width: '45px',
-        height: '45px',
-        borderRadius: '50%',
-        backgroundColor: 'rgba(34, 211, 238, 0.2)',
-        border: '1px solid #22d3ee',
-        color: '#22d3ee',
-        backdropFilter: 'blur(10px)',
-        zIndex: 9999,
-        cursor: 'pointer',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontSize: '20px'
-      }}
-    >
-      ↻
-    </button> */}
             </Link>
           );
         })}
