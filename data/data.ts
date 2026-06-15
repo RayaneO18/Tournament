074731c12e7b5ea26b3779800678089066174536
Ouaani Rayane <ouaani.rayane@gmail.com>
2026-06-15 19:38:06 +0200

    ajout de tab pour match du fair play et match retour parents

--- data/data.ts
+++ data/data.ts
@@ -136,6 +136,6 @@ export const initialPetiteFinale: Match = { eq1: "", score1: null, tab1: null, e
 
 export const initialMatchsSpeciaux: Match[] = [
   { eq1: "Animateurs", score1: null, eq2: "Parents", score2: null, poule: "Spécial", date: "19 Juin", label: "MATCH ALLER ANIMATEURS VS PARENTS" },
-  { eq1: "Algérie", score1: null, eq2: "Portugal", score2: null, poule: "Spécial", date: "22 Juin", label: "COUPE DU FAIR-PLAY" },
-  { eq1: "Parents", score1: null, eq2: "Animateurs", score2: null, poule: "Spécial", date: "25 Juin", label: "MATCH RETOUR ANIMATEURS VS PARENTS" }
+  { eq1: "Algérie", score1: null, tab1: null, eq2: "Portugal", score2: null, tab2: null, poule: "Spécial", date: "22 Juin", label: "COUPE DU FAIR-PLAY" },
+  { eq1: "Parents", score1: null, tab1: null, eq2: "Animateurs", score2: null, tab2: null, poule: "Spécial", date: "25 Juin", label: "MATCH RETOUR ANIMATEURS VS PARENTS" }
 ];
